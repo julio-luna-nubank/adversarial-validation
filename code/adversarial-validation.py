c42641f29baed2f4dfa25508c93fa7977972f4ba
multivac <jlqmoreno@gmail.com>
2022-07-07 14:05:55 -0500

    added plot_shap and recursive_feature_elimination functions

--- code/adversarial-validation.py
+++ code/adversarial-validation.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import numpy as np
 
+from copy import deepcopy
+
 from typing import Dict, Callable
 
 from sklearn.model_selection import train_test_split
 
 import shap
+
+import tqdm
 
 
 
@@ -59,6 +63,10 @@ class AdversarialValidation:
 
 		self.performance_ = roc_auc_score(y_true = y_test, y_score = predictions)
 
+		explainer = shap.TreeExplainer(model = self.base_estimator)
+		self.shap_values = explainer.shap_values(X_test)
+		
+
 		return self
 
 	def predict(self, frame):
@@ -81,11 +89,55 @@ class AdversarialValidation:
 
 		"""
 
-		_, X, _, _ = self._make_split(frame)
+		shap.summary_plot(self.shap_values, feature_names = self.features, plot_type = plot_type)
 
-		explainer = shap.TreeExplainer(model = self.base_estimator)
-		shap_values = explainer.shap_values(X)
-		shap.summary_plot(shap_values, feature_names = self.features, plot_type = plot_type)
+
+	def recursive_feature_elimination(self,frame:pd.DataFrame, n_features_remove:int = None, threshold_remove_until:float = .5):
+
+		"""
+
+		"""
+		ldf = list()
+		tmp_performance:float = 1.0
+
+		all_features_shap = pd.DataFrame({"average_shap_value":np.abs(self.shap_values).mean()}, index = self.features).sort_values(ascending = False)
+		all_features_adversarial_performance = self.performance_
+		most_important_feature:str = all_features_shap.idxmax()
+
+		ldf.append(("all_features",all_features_adversarial_performance))
+
+		tmp_features = self.features[:]
+
+		if n_features_remove is not None:
+			for iter in tqdm(range(n_features_remove)):
+				tmp_frame = deepcopy(frame)
+				tmp_frame.drop(most_important_feature, axis = 1, inplace = True)
+				tmp_features.remove(most_important_feature)
+				tmp_estimator = self.fit(tmp_frame)
+
+				tmp_features_shap = pd.DataFrame({"average_shap_value":np.abs(self.shap_values).mean()}, index = tmp_features).sort_values(ascending = False)
+				most_important_feature:str = all_features_shap.idxmax()
+				tmp_performance = self.performance_
+
+				ldf.append((most_important_feature,tmp_performance))
+		elif threshold_remove_until is not None:
+			while (tmp_performance > threshold_remove_until) and len(tmp_features) > 0:
+				tmp_frame = deepcopy(frame)
+				tmp_frame.drop(most_important_feature, axis = 1, inplace = True)
+				tmp_features.remove(most_important_feature)
+				tmp_estimator = self.fit(tmp_frame)
+
+				tmp_features_shap = pd.DataFrame({"average_shap_value":np.abs(self.shap_values).mean()}, index = tmp_features).sort_values(ascending = False)
+				most_important_feature:str = all_features_shap.idxmax()
+				tmp_performance = self.performance_
+
+				ldf.append((most_important_feature,tmp_performance))
+
+
+		return pd.DataFrame(ldf, columns = ["columns", "adversarial_model_performance"])
+
+
+
 
 
 
